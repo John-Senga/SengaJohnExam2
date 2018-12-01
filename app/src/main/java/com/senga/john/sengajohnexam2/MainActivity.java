e0f0b9aa9732dc286668ffd4f48dad1257dc8746
cisco <cisco@IICSGENERAL.com>
2018-12-01 13:31:39 +0800

    Added function

--- app/src/main/java/com/senga/john/sengajohnexam2/MainActivity.java
+++ app/src/main/java/com/senga/john/sengajohnexam2/MainActivity.java
@@ -2,12 +2,20 @@ package com.senga.john.sengajohnexam2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText fname, lname, exam1, exam2, ave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        exam1 = findViewById(R.id.exam1);
+        exam2 = findViewById(R.id.exam2);
+        ave = findViewById(R.id.ave);
     }
 }
